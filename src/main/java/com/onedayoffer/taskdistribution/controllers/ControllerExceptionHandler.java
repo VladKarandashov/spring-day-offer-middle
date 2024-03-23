76c8505c9dd839244900d62967602fd26ea474ef
karandashov <vlad.karandashov.tech@gmail.com>
2024-03-23 11:02:01 +0300

    причесал

--- src/main/java/com/onedayoffer/taskdistribution/controllers/ControllerExceptionHandler.java
+++ src/main/java/com/onedayoffer/taskdistribution/controllers/ControllerExceptionHandler.java
@@ -1,11 +1,31 @@
 package com.onedayoffer.taskdistribution.controllers;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import com.onedayoffer.taskdistribution.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@ControllerAdvice(annotations= RestController.class)
+@Slf4j
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    ResponseEntity<String> handle(BusinessException e) {
+        log.error("Возникло бизнес-исключение: ", e);
+        var response = e.toString();
+        return ResponseEntity.internalServerError()
+                .body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    ResponseEntity<String> handle(Exception e) {
+        log.error("Возникло непредвиденное исключение: ", e);
+        var response = e.getMessage();
+        return ResponseEntity.internalServerError()
+                .body(response);
+    }
 
 }
--- src/main/java/com/onedayoffer/taskdistribution/services/EmployeeService.java
+++ src/main/java/com/onedayoffer/taskdistribution/services/EmployeeService.java
@@ -32,42 +32,30 @@ public class EmployeeService {
     private final ModelMapper modelMapper;
 
     public List<EmployeeDTO> getEmployees(@Nullable String sortDirection) {
-        List<Employee> employeeList;
-        if (StringUtils.isNotBlank(sortDirection)) {
-            Sort.Direction direction = Sort.Direction.fromOptionalString(sortDirection).orElseThrow(() -> {
-                log.error("Не правильно задан порядок сортировки {} (ASС/DESC)", sortDirection);
-                return new BusinessException(ExceptionStatus.VALIDATION_ERROR, "Порядок сортировки должен быть ASС/DESC");
-            });
-            employeeList = employeeRepository.findAllAndSort(Sort.by(direction, "fio"));
-        } else {
-            employeeList = employeeRepository.findAll();
-        }
+        Sort sort = getSortByEmployeeFio(sortDirection);
+        List<Employee> employeeList = employeeRepository.findAllAndSort(sort);
+        log.info("Найдено {} записей Employee", employeeList.size());
         Type listType = new TypeToken<List<EmployeeDTO>>() {}.getType();
         return modelMapper.map(employeeList, listType);
     }
 
     @Transactional
     public EmployeeDTO getOneEmployee(Integer id) {
-        Employee employee = employeeRepository.findById(id).orElseThrow(() -> {
-            log.error("Не найден employee с id = {}", id);
-            return new BusinessException(ExceptionStatus.NOT_FOUND_EMPLOYEE);
-        });
+        Employee employee = getEmployeeById(id);
         log.info("Успешно найден employee с id = {}", id);
         return modelMapper.map(employee, EmployeeDTO.class);
     }
 
     public List<TaskDTO> getTasksByEmployeeId(Integer id) {
-        List<Task> taskListByEmployeeId =  taskRepository.findAllByEmployeeId(id);
+        List<Task> taskListByEmployeeId = taskRepository.findAllByEmployeeId(id);
+        log.info("Найдено {} записей Task", taskListByEmployeeId.size());
         Type listType = new TypeToken<List<TaskDTO>>() {}.getType();
         return modelMapper.map(taskListByEmployeeId, listType);
     }
 
     @Transactional
     public void changeTaskStatus(Integer employeeId, Integer taskId, TaskStatus status) {
-        Task task = taskRepository.findById(taskId).orElseThrow(() -> {
-            log.error("Не найдена Task с id = {}", taskId);
-            return new BusinessException(ExceptionStatus.NOT_FOUND_TASK);
-        });
+        Task task = getTaskById(taskId);
         task.setStatus(status);
         taskRepository.save(task);
     }
@@ -75,11 +63,33 @@ public class EmployeeService {
     @Transactional
     public void postNewTask(Integer employeeId, TaskDTO newTask) {
         Task task = modelMapper.map(newTask, Task.class);
-        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> {
+        Employee employee = getEmployeeById(employeeId);
+        task.setEmployee(employee);
+        taskRepository.save(task);
+    }
+
+    private Sort getSortByEmployeeFio(@Nullable String sortDirection) {
+        if (StringUtils.isBlank(sortDirection)) {
+            return Sort.unsorted();
+        }
+        Sort.Direction direction = Sort.Direction.fromOptionalString(sortDirection).orElseThrow(() -> {
+            log.error("Не правильно задан порядок сортировки {} (ASС/DESC)", sortDirection);
+            return new BusinessException(ExceptionStatus.VALIDATION_ERROR, "Порядок сортировки должен быть ASС/DESC");
+        });
+        return Sort.by(direction, "fio");
+    }
+
+    private Employee getEmployeeById(Integer employeeId) {
+        return employeeRepository.findById(employeeId).orElseThrow(() -> {
             log.error("Не найден employee с id = {}", employeeId);
             return new BusinessException(ExceptionStatus.NOT_FOUND_EMPLOYEE);
         });
-        task.setEmployee(employee);
-        taskRepository.save(task);
+    }
+
+    private Task getTaskById(Integer taskId) {
+        return taskRepository.findById(taskId).orElseThrow(() -> {
+            log.error("Не найдена Task с id = {}", taskId);
+            return new BusinessException(ExceptionStatus.NOT_FOUND_TASK);
+        });
     }
 }
